3d27ae9c9ed3c9f54362e4edcfaa5173a6536c4c
mnbf9rca <robert.aleck@cynexia.com>
2019-01-08 16:17:23 +0000

    rename folder to stop confusing myself

--- 3D_perception_project/src/sensor_stick/scripts/capture_features.py
+++ 3D_perception_project/src/sensor_stick/scripts/capture_features.py
@@ -24,13 +24,14 @@ if __name__ == '__main__':
     rospy.init_node('capture_node')
 
     models = [\
-       'beer',
-       'bowl',
-       'create',
-       'disk_part',
-       'hammer',
-       'plastic_cup',
-       'soda_can']
+       'sticky_notes',
+       'book',
+       'snacks',
+       'biscuits',
+       'eraser',
+       'soap2',
+       'soap',
+       'glue']
 
     # Disable gravity and delete the ground plane
     initial_setup()
@@ -64,5 +65,5 @@ if __name__ == '__main__':
         delete_model()
 
 
-    pickle.dump(labeled_features, open('training_set.sav', 'wb'))
+    pickle.dump(labeled_features, open('training_set_world_3.sav', 'wb'))
 
--- RoboND-Perception-Project/devel/share/pr2_robot/cmake/pr2_robot-msg-extras.cmake
+++
@@ -1,2 +0,0 @@
-set(pr2_robot_MESSAGE_FILES "")
-set(pr2_robot_SERVICE_FILES "/home/robond/catkin_ws/src/RoboND-Perception-Project/pr2_robot/srv/PickPlace.srv;/home/robond/catkin_ws/src/RoboND-Perception-Project/pr2_robot/srv/Grasp.srv")
